f9790732b940111bbfc6df1ffd0e9fc44cef5fd8
Mevent <bestolo4@bk.ru>
2020-12-06 15:44:24 +0300

    Добавьте файлы проекта.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,121 @@
+﻿
+namespace MathChart
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.label1 = new System.Windows.Forms.Label();
+            this.X0Label = new System.Windows.Forms.TextBox();
+            this.XNLabel = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(0, 26);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(586, 390);
+            this.cartesianChart1.TabIndex = 1;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(20, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "X0";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // X0Label
+            // 
+            this.X0Label.Location = new System.Drawing.Point(35, 6);
+            this.X0Label.Name = "X0Label";
+            this.X0Label.Size = new System.Drawing.Size(100, 20);
+            this.X0Label.TabIndex = 3;
+            // 
+            // XNLabel
+            // 
+            this.XNLabel.Location = new System.Drawing.Point(164, 6);
+            this.XNLabel.Name = "XNLabel";
+            this.XNLabel.Size = new System.Drawing.Size(100, 20);
+            this.XNLabel.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(141, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(22, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "XN";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(270, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 23);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Построить график";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(586, 416);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.XNLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.X0Label);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "График функции Sin(x)";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox X0Label;
+        private System.Windows.Forms.TextBox XNLabel;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,40 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Windows.Forms;
+
+namespace MathChart
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var minValue = 0;
+            int.TryParse(X0Label.Text, out minValue);
+
+            var maxValue = 100;
+            int.TryParse(XNLabel.Text, out maxValue);
+
+            var values = new ChartValues<double>();
+
+            for (var x = minValue; x <= maxValue; x++)
+            {
+                values.Add(Math.Sin(x));
+            }
+
+            cartesianChart1.Series.Clear();
+            cartesianChart1.Series.Add(new LineSeries
+            {
+                Values = values
+            });
+
+            cartesianChart1.AxisX[0].MinValue = minValue;
+            cartesianChart1.AxisX[0].MaxValue = maxValue;
+        }
+    }
+}
